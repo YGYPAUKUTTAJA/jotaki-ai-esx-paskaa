5869016ef0c020eb479956253a533dcf517b16a4
YGYPAUKUTTAJA <ville.t.niskakangas@gmail.com>
2025-02-05 19:14:56 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'bodacious'
+game 'gta5'
+
+client_scripts {
+    'config.lua',  -- Varmista, että tämä on listattu ennen client.lua
+    'client.lua'
+}
+
+server_scripts {
+    'config.lua',  -- Varmista, että tämä on listattu ennen server.lua
+    'server.lua'
+}
